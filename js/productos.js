0ae5d838c696019b20a5dfbf9d2ff986039ee12a
Said Gama <sgamap8@gmail.com>
2023-04-19 15:48:17 -0600

    se agregan productos

--- js/productos.js
+++ js/productos.js
@@ -1,28 +1,19 @@
-let producto1 = [{ "id": 1, "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", "price": 109.95, "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday", "category": "men's clothing", "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", "rating": { "rate": 3.9, "count": 120 } }]
-//id, precio, nombre, descripcion, stock, imagen,  imagen1,imagen2,imagen3,imagen4, descripcion1, detallestecnicos}
-ordenar de A-Z
-ordenar de menor mayor precio
-ordenar de mayor a menor precio
-
-
-function addItem(item){
-    const itemHTML = '<div class="card" style="width: 18rem;">\n' +
-        '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
-        '    <div class="card-body">\n' +
-        '        <h5 class="card-title">'+item.name+'</h5>\n' +
-        '        <p class="card-text">'+item.description+'</p>\n' +
-        '        <a href="#" class="btn btn-primary">Add</a>\n' +
-        '    </div>\n' +
-        '</div>\n' +
-        '<br/>';
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
+function addItem(item) {
+    const itemHTML = `
+    <div class = "cajitas">
+    <div class="card h-100" style="width: 18rem;">
+        <img src="${item.imagen['img1']}" class="card-img-top" alt="imagen">
+        <div class="card-body">
+            <h5 class="card-title">${item.nombre}</h5>
+            <p class="card-text">${item.descripcion}</p>
+            <a href="#" class="btn btn-primary">Agregar al carrito</a>
+            <h5 class="card-title">${item.precio}</h5>
+        </div>
+    </div>
+    </div>`;
+    const productosDiv = document.getElementById("productosDiv");
+    productosDiv.innerHTML += itemHTML;
 }
 
-addItem({'name':'juice',
-    'img':'https://www.gs1india.org/media/Juice_pack.jpg',
-    'description':'Orange and Apple juice fresh and delicious'});
-
-addItem({'name':'Tayto',
-    'img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg',
-    'description':'Cheese & Onion Chips'})
+let produc1 = { "id": 1, "nombre": "Garrafón de Agua - 20lts", "precio": 20.00, "descripcion": {"descripcion1": "#", "detallesTec": "#"}, "imagen": {"img1": "./imagenesProductos/enfriador.jpg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
+addItem(produc1);
