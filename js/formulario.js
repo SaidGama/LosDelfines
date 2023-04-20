c15675c75a2ff353c835628c4fb044492a4f1003
Jaz <jaz.islasg@gmail.com>
2023-04-20 16:42:03 -0600

    Se ingresa el id de descripción en html y en js/formulario se escribe la validación para que el campo no este vacio y tenga un maximo de 50 caracteres

--- js/formulario.js
+++ js/formulario.js
@@ -1,3 +1,4 @@
+let campDescripcion = document.getElementById("campDescrip");
 
 function validacionCampoID(params) {
     
@@ -9,8 +10,14 @@ function validacionPrecio(params) {
     
 }
 function validacionDescripcion(params) {
-    
-}
+    if (campDescripcion.value.length<1 || campDescripcion.value.length>50){
+        campDescripcion.style.border="solid thin red";
+    }else {
+        campDescripcion.style.border="solid thin green";
+        return true;
+    }//validar la descripción breve con uno y menos de 50 caracteres
+}//validacionDescripción 
+
 function validacionDetallestecnicos(params) {
     
 }
