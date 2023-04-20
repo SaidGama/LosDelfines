a6e74c006a69cd48469321b0e8a16eb40472d093
Said Gama <sgamap8@gmail.com>
2023-04-20 16:02:08 -0600

    se creo el formulario

---
+++ js/formulario.js
@@ -0,0 +1,20 @@
+
+
+function validacionCampoID(params) {
+    
+}
+function validacionCampoNombre(params) {
+    
+}
+function validacionPrecio(params) {
+    
+}
+function validacionDescripcion(params) {
+    
+}
+function validacionDetallestecnicos(params) {
+    
+}
+function validacionStock(params) {
+    
+}
