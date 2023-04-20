5d0dd730017c3f52d087f2aa75b3851f45f56002
jair <jair.ibarra.p@gmail.com>
2023-04-20 16:58:42 -0600

    Se agregó la validacion del nombre del producto

--- js/formulario.js
+++ js/formulario.js
@@ -1,10 +1,17 @@
+let campNombre = document.getElementById("campNombre");
+
 
 function validacionCampoID(params) {
     
 }
 function validacionCampoNombre(params) {
-    
-}
+    if (campNombre.ariaValueMax.length <4 || campNombre.ariaValueMax.length>50){
+        campNombre.style.border="solid thin red";
+    }else{
+        campNombre.style.border="solid thin green";
+        return true;
+    }//if
+}//validar Nombre
 function validacionPrecio(params) {
     
 }
@@ -17,3 +24,20 @@ function validacionDetallestecnicos(params) {
 function validacionStock(params) {
     
 }
+
+
+
+campNombre.addEventListener("blur", function(event){
+    event.preventDefault();
+    campNombre.value = campNombre.value.trim();
+}); //blur
+
+campDescripcion.addEventListener("blur", function(event){
+    event.preventDefault();
+    campDescripcion.value = campDescripcion.value.trim();
+}); //blur
+
+Id.addEventListener("blur", function(event){
+    event.preventDefault();
+    Id.value = Id.value.trim();
+}); //blur
