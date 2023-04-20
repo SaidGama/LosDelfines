7191dd688b9a6271170491d31328504d01ed5c22
ManuelAmaya <manuel.amaya.acosta@hotmail.com>
2023-04-20 17:29:19 -0600

    Se crear botonCrear y condicional

--- js/formulario.js
+++ js/formulario.js
@@ -1,19 +1,45 @@
+let btnCrear = document.getElementById("botonCrear");
+let arrayProductos = [];
+
+
+
+btnCrear.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (validacionCampoNombre() == true && validacionPrecio() == true && validacionDescripcion() == true
+        && validacionDetallestecnicos() == true && validacionStock()) {
+        let producto = `{
+             "id": ${}, 
+             "nombre": "${}", 
+             "precio": ${}, 
+             "descripcion": 
+             {"descripcion1": "${}", 
+             "detallesTec": " "}, 
+             "imagen": 
+             {"img1": "./productos_ima/garrafones cuadrados 20 litros.jpeg", 
+             "img2": "#", 
+             "img3": "#" , 
+             "img4": "#"}, 
+             "stock": 1}`
+    }
+})
 
 function validacionCampoID(params) {
-    
+
 }
 function validacionCampoNombre(params) {
-    
+
 }
 function validacionPrecio(params) {
-    
+
 }
 function validacionDescripcion(params) {
-    
+
 }
 function validacionDetallestecnicos(params) {
-    
+
 }
 function validacionStock(params) {
-    
+
 }
+
