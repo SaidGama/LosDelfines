f1847b4f9db29018924d2f25d3007efc5a089f16
Said Gama <sgamap8@gmail.com>
2023-04-26 12:30:54 -0600

    correciones

--- js/productos.js
+++ js/productos.js
@@ -1,4 +1,4 @@
-/*let produc1 = { "id": 1, "nombre": "Garrafón de Agua - 20lts", "precio": 25.00, "descripcion": {"descripcion1": "Relleno de garrafón con 20 lts de agua purificada", "detallesTec": "Rellenado de garrafón de 20 lts con agua purificada de alta calidad (Servicio en tienda o con entrega a domicilio)."}, "imagen": {"img1": "./productos_ima/garrafones cuadrados 20 litros.jpeg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
+let produc1 = { "id": 1, "nombre": "Garrafón de Agua - 20lts", "precio": 25.00, "descripcion": {"descripcion1": "Relleno de garrafón con 20 lts de agua purificada", "detallesTec": "Rellenado de garrafón de 20 lts con agua purificada de alta calidad (Servicio en tienda o con entrega a domicilio)."}, "imagen": {"img1": "./productos_ima/garrafones cuadrados 20 litros.jpeg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
 let produc2 = { "id": 2, "nombre": "Agua - 5lts", "precio": 27.50, "descripcion": {"descripcion1": "Botellón de 5lts con agua purificada", "detallesTec": "Botellón de 5lts desechable con agua purificada de alta calidad (Venta en tienda o con entrega a domicilio)."}, "imagen": {"img1": "./productos_ima/garrafon azul y rosa cuadricula se queda.jpeg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
 let produc3 = { "id": 3, "nombre": "Agua - Pack 6pzs 1lt", "precio": 39.99,"descripcion": {"descripcion1": "Paquete con 6 botellas de 1lt (venta en tienda o entrega a domicilio).", "detallesTec": "Cada paquete contiene 6 botellas desechables de 1 lt  cada una.(Solo entrega a domicilio)"}, "imagen": {"img1": "./productos_ima/embotellada 1 litro.jpg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
 let produc4 = { "id": 4, "nombre": "Agua - Pack 28pzs 500ml", "precio": 75.00, "descripcion": {"descripcion1": "Paquete con 28 botellas de 500 ml", "detallesTec": "Cada paquete contiene 28 botellas desechables de 500 ml cada una.(Solo entrega a domicilio)."}, "imagen": {"img1": "./productos_ima/botella500ml.jpeg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
@@ -9,7 +9,7 @@ let produc8 = { "id": 8, "nombre": "Dispensador de agua clásico", "precio": 169
 let produc9 = { "id": 9, "nombre": "Dispensador de agua eléctrico", "precio": 149.99, "descripcion": {"descripcion1": "Dispensador eléctrico portátil de agua.", "detallesTec": "Dispensador de agua electrico portatil para garrafón, recargable mediante entrada USB, de fácil instalación"}, "imagen": {"img1": "./productos_ima/dispensador de agua para garrafon.jpg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
 let produc10 = { "id": 10, "nombre": "Enfriador", "precio": 1190.00, "descripcion": {"descripcion1": "Enfriador de agua.", "detallesTec": "Base para garrafón dispensadora de agua, almacena y enfría el agua, dispensador en columna."}, "imagen": {"img1": "./imagenesProductos/enfriador.jpg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
 let produc11 = { "id": 11, "nombre": "Botellón pet plástico 20lts", "precio": 110.00, "descripcion": {"descripcion1": "Botellón PET con 20 lts de capacidad.", "detallesTec": "Botellón Plástico PET desechable con 20 lts de capacidad."}, "imagen": {"img1": "./productos_ima/garrafon tamaño cuadricula se queda.jpeg", "img2": "#", "img3": "#" , "img4": "#"}, "stock": 1};
-*/
+
 let arrayProductos = [];
 
 
@@ -42,7 +42,7 @@ function addItem(item) {
     productosDiv.innerHTML += itemHTML;
 }
 
-/*
+
 addItem(produc1);
 addItem(produc2);
 addItem(produc3);
@@ -53,4 +53,4 @@ addItem(produc7);
 addItem(produc8);
 addItem(produc9);
 addItem(produc10);
-addItem(produc11);*/
+addItem(produc11);
