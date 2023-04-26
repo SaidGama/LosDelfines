7ec5635e5240dba6e2e4f0b1ffcc9cba260e5809
Said Gama <sgamap8@gmail.com>
2023-04-26 12:26:53 -0600

    correciones

--- js/navbarBoton.js
+++ js/navbarBoton.js
@@ -1,16 +1,2 @@
-let botonNavbar = document.getElementById("botonNavbar");
-let containerDivlinks = document.getElementById("containerDivlinks");
-let contador = 0;
 
-    botonNavbar.addEventListener("click", function(event){
-        event.preventDefault();
-        contador++;
-        esPresionado = true;
-        if(contador%2 == 0){
-            containerDivlinks.style.display = "none";
-        }else{
-            containerDivlinks.style.display = "block";
-        }
- 
-    });
 
